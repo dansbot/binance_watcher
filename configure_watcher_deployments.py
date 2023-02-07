cdde821c637399fd8f8e0b3f5fb0783b61f0f0b6
dansbot <danrparis@gmail.com>
2023-02-07 15:51:56 -0800

    it's working

--- configure_watcher_deployments.py
+++ configure_watcher_deployments.py
@@ -2,7 +2,6 @@ from binance.client import Client
 from copy import deepcopy
 import os
 import yaml
-from pprint import pprint
 
 client = Client(
     api_key=os.environ.get("BINANCE_API_KEY"),
@@ -16,11 +15,14 @@ with open('k8s/templates/watcher-deployment-template.yaml') as fp:
 s = ""
 exchange_info = client.get_exchange_info()
 for symbol in exchange_info['symbols']:
+    if 'usd' not in symbol['symbol'].lower():
+        continue
     dep = deepcopy(deployment_template)
+    dep['metadata']['name'] = symbol['symbol'].lower()
     dep['spec']['template']['spec']['containers'][0]['args'] = [symbol['symbol'], 'trade']
     s += yaml.dump(dep)
     s += "---\n"
-    break
+    # break
 
 with open('k8s/watcher-deployments.yaml', 'w') as fp:
     fp.write(s[:-len("---\n")])
